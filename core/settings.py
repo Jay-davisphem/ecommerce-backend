2bc4bc6a36eac7a1342c2e414814a3aeb89d16bd
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-04-06 15:03:46 +0100

    issues: fix images getter issue

--- core/settings.py
+++ core/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@1mvr+z1(8i*&oao&bo5quvrseype5hmgqp)(muhl#hjlv9f+@'
+SECRET_KEY = "django-insecure-@1mvr+z1(8i*&oao&bo5quvrseype5hmgqp)(muhl#hjlv9f+@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -19,59 +19,61 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'foodie1',
-    'rest_framework.authtoken',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "foodie1",
+    "rest_framework.authtoken",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'projs',
-        'USER': 'davisphem',
-        'PASSWORD': 'phemmy2022',
-        'HOST': '127.0.0.1',
-        'PORT': '',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "projs",
+        "USER": "davisphem",
+        "PASSWORD": "phemmy2022",
+        "HOST": "127.0.0.1",
+        "PORT": "",
     }
 }
 
@@ -80,16 +82,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -97,33 +99,37 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Africa/Lagos'
+TIME_ZONE = "Africa/Lagos"
 
 USE_I18N = True
 
 USE_TZ = True
 
-LOGIN_REDIRECT_URL = 'foodie:api-root'
+LOGIN_REDIRECT_URL = "foodie:api-root"
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-REST_FRAMEWORK = {                                                                                         
-      'DEFAULT_AUTHENTICATION_CLASSES': (                                                       
-          'rest_framework.authentication.TokenAuthentication',                                          
-          'rest_framework.authentication.SessionAuthentication',                                    
-          ),                                                                                            
-      'DEFAULT_PERMISSION_CLASSES': (                                                                   
-          'rest_framework.permissions.IsAuthenticated',                                         
-          ),                                                                                
-      # 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'                             
-      }   
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    # 'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
+}
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+]
--- foodie1/serializers.py
+++ foodie1/serializers.py
@@ -4,54 +4,92 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import User
 
 
+class FoodImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.FoodImage
+        fields = "__all__"
+
+
+class MeatImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.MeatImage
+        fields = ("food_images", "is_feature")
+
+
 class FoodSerializer(serializers.ModelSerializer):
+    food_images = FoodImageSerializer(many=True, read_only=True)
+    print(food_images, "lovi lovi")
+
     class Meta:
         model = models.Food
-        fields = '__all__'
+        fields = [
+            "id",
+            "name",
+            "description",
+            "regular_price",
+            "discount_price",
+            "vendor",
+            "category",
+            "meats",
+            "food_images",
+        ]
 
 
 class MeatSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Meat
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Category
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Customer
-        exclude = ['is_staff', 'is_active', 'is_superuser',
-'last_login', 'date_joined', 'user_permissions', 'groups']
+        exclude = [
+            "is_staff",
+            "is_active",
+            "is_superuser",
+            "last_login",
+            "date_joined",
+            "user_permissions",
+            "groups",
+        ]
 
 
 class VendorSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Vendor
-        exclude = ['password', 'is_staff', 'is_active', 'is_superuser',
-                   'last_login', 'date_joined', 'groups']
+        exclude = [
+            "password",
+            "is_staff",
+            "is_active",
+            "is_superuser",
+            "last_login",
+            "date_joined",
+            "groups",
+        ]
 
 
 class CustomerLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CustomerLogin
-        fields = '__all__'
+        fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ("username", "email", "password")
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User(email=validated_data['email'],
-                    username=validated_data['username'])
-        user.set_password(validated_data['password'])
+        user = User(email=validated_data["email"], username=validated_data["username"])
+        user.set_password(validated_data["password"])
         user.save()
         Token.objects.create(user=user)
         return user
@@ -60,58 +98,46 @@ class UserSerializer(serializers.ModelSerializer):
 class VendorLoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.VendorLogin
-        fields = '__all__'
-
-
-class FoodImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.FoodImage
-        fields = '__all__'
-
-
-class MeatImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.MeatImage
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CartItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.CartItem
-        fields = '__all__'
+        fields = "__all__"
 
 
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Cart
-        exclude = ['user']
+        exclude = ["user"]
 
 
 class OrderStatusCodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.OrderStatusCode
-        fields = '__all__'
+        fields = "__all__"
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Order
-        fields = '__all__'
+        fields = "__all__"
 
 
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.OrderItem
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ShipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Shipment
-        fields = '__all__'
+        fields = "__all__"
 
 
 class ShipmentItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.ShipmentItem
-        fields = '__all__'
+        fields = "__all__"
